a8c12d7e93308db37e6b70cc82688a66ab436a30
“riham” <re.khalaily@gmail.com>
2022-02-16 13:10:25 +0200

    router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 
 import './App.css';
-
+import {
+  BrowserRouter,
+  Routes,
+  Route
+}from "react-router-dom";
+import Home from './pages/Home';
+import Admin from './pages/Admin';
 function App() {
   return (
-    <div className="App">
-      hello
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/admin' element={<Admin />}/>
+
+    </Routes>
+    </BrowserRouter>
+    
   );
 }
 

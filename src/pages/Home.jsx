43f8321123d32de6bcf8a7682dad85ac85f07706
“riham” <re.khalaily@gmail.com>
2022-03-06 13:40:56 +0200

    remove function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,29 +4,37 @@ import "../App.css";
 // import { FaPlus } from "react-icons/fa";
 
 function Home(props) {
-    return (
-        <div>
-            <nav className="buttons">
-                {/* <a id="home" className="homeButton" href="/">Home</a> */}
+  return (
+    <div>
+      <nav className="buttons">
+        {/* <a id="home" className="homeButton" href="/">Home</a> */}
 
-                <a id="logout" className="logoutButton" href="/">logout</a>
-                <a id="allprojects" className="allprojectsButton" href="/home">all projects</a>
+        <a id="logout" className="logoutButton" href="/">
+          logout
+        </a>
+        <a id="allprojects" className="allprojectsButton" href="/home">
+          all projects
+        </a>
 
-                <a id="completed" className="completedButton" href="/home">completed</a>
-                <a id="uncompleted" className="uncompleted" href="/home">uncompleted</a>
-            </nav>
-            <h1>
-                Project Management
-            </h1>
-            
-            <a  href="/addProject"> <img className="addImg" src="https://cdn3.iconfinder.com/data/icons/rcons-file-action/512/add_file-512.png"></img></a>
-           {/* <a  href="/addProject"><FaPlus 
-                style={{ color: "black", cursor: "pointer", fontSize:"large" }}
-                
-              /></a> */}
-{/* <a  href="/addProject">Add Project</a> */}
-            <Project></Project>
-        </div>
-    )
+        <a id="completed" className="completedButton" href="/home">
+          completed
+        </a>
+        <a id="uncompleted" className="uncompleted" href="/home">
+          uncompleted
+        </a>
+      </nav>
+      <h1>Project Management</h1>
+
+      <a href="/addProject">
+        {" "}
+        <img
+          className="addImg"
+          src="https://cdn3.iconfinder.com/data/icons/rcons-file-action/512/add_file-512.png"
+        ></img>
+      </a>
+
+      <Project></Project>
+    </div>
+  );
 }
 export default Home;
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,76 +1,77 @@
 import "../App.css";
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { FaTimes } from "react-icons/fa";
 function Project() {
-    const [projectData, setProjectData] = useState([])
-    const prijectsArr = projectData.filter((v, i, a) => a.findIndex(t => (t.id === v.id)) === i);
-    // const removeItem = (name) => {
-    //     setProjectData(
-    //         projectData.filter((element) => {
-    //             return element.project_name !== name;
-    //         })
-    //     );
-    // };
-    const deleteTableRows = (index)=>{
-        const rows = [...prijectsArr];
-        rows.splice(index, 1);
-        setProjectData(rows);
-        fetch('http://localhost:4000/project', {
-            method: "post",
-            body: JSON.stringify(projectData),
-            headers: { 'content-type': 'application/json' },
-            credentials: "include"
+  const [projectData, setProjectData] = useState([]);
+  const prijectsArr = projectData.filter(
+    (v, i, a) => a.findIndex((t) => t.id === v.id) === i
+  );
 
-        })
-        .then(res => res.json()).then(data => {
-            if (data.delete === true) {
-                alert("project deleted");
-                window.location.href = "http://localhost:3000/home";
-            } })
-   }
- 
+  const deleteTableRows = (project_name) => {
+    setProjectData(
+      projectData.filter((obj) => {
+        if (obj.project_name === project_name) return false;
 
-    useEffect(() => {
-        fetch('http://localhost:4000/projectsInfo')
-            .then(res => res.json()).then(data => {
-                setProjectData(data)
-            })
+        return true;
+      })
+    );
 
-    }, [])
+    fetch(`http://localhost:4000/delete/${project_name}`, {
+      method: "DELETE",
+      credentials: "include",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.delete === true) {
+          alert("project deleted");
+          window.location.href = "http://localhost:3000/home";
+        } else console.log("else");
+      });
+  };
 
+  useEffect(() => {
+    fetch("http://localhost:4000/projectsInfo")
+      .then((res) => res.json())
+      .then((data) => {
+        setProjectData(data);
+        console.log(data);
+      });
+  }, []);
 
-    return (
-        <div className="projectStyle">
+  return (
+    <div className="projectStyle">
+      {prijectsArr.map((project, index) => {
+        return (
+          <div class="column">
+            <div class="card">
+              <div>
+                <FaTimes
+                  style={{ color: "black", cursor: "pointer" }}
+                  onClick={() => deleteTableRows(project.project_name)}
+                />
+                <h3> {project.project_name}</h3>
 
-            {prijectsArr.map(project => {
-                return (
-                    <div class="column">
-                    <div class="card">
-                  
-                    <div>
-                         <FaTimes
-                style={{ color: "black", cursor: "pointer" }}
-                onClick={() => deleteTableRows(project.name)}
-              />
-                        <h3> {project.project_name}</h3>
-
-                        <div>Start at: {project.start_at}</div>
-                        <div>End at:  {project.end_at}</div>
-                        <div><h4 class="taskTitle">Tasks</h4>
-                            {projectData.map((pro) => {
-                                if (pro.project_name === project.project_name) return <li>{pro.task_name}</li>
-                            })}
-
-                        </div>
-                        <div  > <button className="doneButton" >Done</button></div>
-                        
-                    </div>
-                    </div>
-                    </div>)
-            })}
-
-        </div>
-    )
+                <div>Start at: {project.start_at}</div>
+                <div>End at: {project.end_at}</div>
+                <div>
+                  <h4 class="taskTitle">Tasks</h4>
+                  {projectData.map((pro) => {
+                    if (pro.project_name === project.project_name)
+                      return <li>{pro.task_name}</li>;
+                  })}
+                </div>
+                <div>
+                  {" "}
+                  <button className="doneButton">Done</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Project;
